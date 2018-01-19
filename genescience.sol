47615b98ba3882c53fc6ae7f116b3f0da011edf5
chengsilei <chengsilei@qq.com>
2018-01-19 14:46:19 +0800

    update gene

--- genescience.sol
+++ genescience.sol
@@ -7,7 +7,7 @@ contract GeneScience {
     mapping (uint32 => uint32[]) _attribute;
     mapping (uint32 => uint8[]) _attribute_mixrule;
     mapping (uint32 => uint32) _variation_rate;						// 千分之几
-    mapping (uint32 => uint32[]) _variation_rate_distribution;		// 百分之几
+    mapping (uint32 => uint32[]) _variation_rate_distribution;		// 万分之几
     
     function GeneScience() public {
         _startTime = now;
@@ -78,27 +78,27 @@ contract GeneScience {
         _variation_rate[19] = 100;
         _variation_rate[20] = 100;
 
-        _variation_rate_distribution[0] = [100,0];                              // 是否EXCLUSIVE
-        _variation_rate_distribution[1] = [100,0];                              // 是否FANCY
-        _variation_rate_distribution[2] = [25,25,25,25];                     	// FANCY属性
-        _variation_rate_distribution[3] = [30,40,30];                           // EXCLUSIVE属性
-        _variation_rate_distribution[4] = [6,6,6,6,6,6,6,6,6,6,6,6,6,6,16]; 	// BODY
-        _variation_rate_distribution[5] = [10,10,10,10,10,10,10,10,10,10];      // TAIL
-        _variation_rate_distribution[6] = [10,10,10,10,10,10,10,10,10,10];    	// PRIMARY COLOR
-        _variation_rate_distribution[7] = [10,10,10,10,10,10,10,10,10,10];      // SECONDARY COLOR
-        _variation_rate_distribution[8] = [10,10,10,10,10,10,10,10,10,5,5];  	// PATTERN TYPE
-        _variation_rate_distribution[9] = [10,10,10,10,10,10,10,10,10,10];   	// PATTERN COLOR
-        _variation_rate_distribution[10] = [10,10,10,10,10,10,10,10,10,10];     // EYE TYPE
-        _variation_rate_distribution[11] = [10,10,10,10,10,10,10,10,10,5,5];  	// EYE COLOR / BACKGROUND COLOR
-        _variation_rate_distribution[12] = [10,10,10,10,10,10,10,10,10,10];     // MOUTH
-        _variation_rate_distribution[13] = [10,10,10,10,10,10,10,10,10,10];     // BEARD
-        _variation_rate_distribution[14] = [9,9,9,9,9,9,9,9,9,9,10];  			// ACCESSORIES
-        _variation_rate_distribution[15] = [10,10,30,30,10,10];               	// 生育能力
-        _variation_rate_distribution[16] = [30,40,30];                          // 性格
-        _variation_rate_distribution[17] = [30,40,30];                          // 速度
-        _variation_rate_distribution[18] = [30,40,30];                          // 智商
-        _variation_rate_distribution[19] = [30,40,30];                          // 体力
-        _variation_rate_distribution[20] = [30,40,30];                          // 技能
+        _variation_rate_distribution[0] = [10000,0];                              // 是否EXCLUSIVE
+        _variation_rate_distribution[1] = [10000,0];                              // 是否FANCY
+        _variation_rate_distribution[2] = [2500,2500,2500,2500];                     	// FANCY属性
+        _variation_rate_distribution[3] = [3000,4000,3000];                           // EXCLUSIVE属性
+        _variation_rate_distribution[4] = [10,50,140,400,800,1000,1600,2000,1600,1000,800,400,140,50,10]; 	// BODY
+        _variation_rate_distribution[5] = [20,150,830,1600,2400,2400,1600,830,150,20];      // TAIL
+        _variation_rate_distribution[6] = [20,150,830,1600,2400,2400,1600,830,150,20];    	// PRIMARY COLOR
+        _variation_rate_distribution[7] = [20,150,830,1600,2400,2400,1600,830,150,20];      // SECONDARY COLOR
+        _variation_rate_distribution[8] = [14,105,581,1120,1680,3000,1680,1120,581,105,14];  	// PATTERN TYPE
+        _variation_rate_distribution[9] = [20,150,830,1600,2400,2400,1600,830,150,20];   	// PATTERN COLOR
+        _variation_rate_distribution[10] = [20,150,830,1600,2400,2400,1600,830,150,20];     // EYE TYPE
+        _variation_rate_distribution[11] = [14,105,581,1120,1680,3000,1680,1120,581,105,14];  	// EYE COLOR / BACKGROUND COLOR
+        _variation_rate_distribution[12] = [20,150,830,1600,2400,2400,1600,830,150,20];     // MOUTH
+        _variation_rate_distribution[13] = [20,150,830,1600,2400,2400,1600,830,150,20];     // BEARD
+        _variation_rate_distribution[14] = [900,900,900,900,900,900,900,900,900,900,1000];  			// ACCESSORIES
+        _variation_rate_distribution[15] = [1000,1000,3000,3000,1000,1000];               	// 生育能力
+        _variation_rate_distribution[16] = [3000,4000,3000];                          // 性格
+        _variation_rate_distribution[17] = [3000,4000,3000];                          // 速度
+        _variation_rate_distribution[18] = [3000,4000,3000];                          // 智商
+        _variation_rate_distribution[19] = [3000,4000,3000];                          // 体力
+        _variation_rate_distribution[20] = [3000,4000,3000];                          // 技能
     }
     
     function random() internal view returns (uint256) {
@@ -166,8 +166,8 @@ contract GeneScience {
 
     	uint32[] storage rate_distribution = _variation_rate_distribution[attID];
     	
-    	uint32 rd = uint32(random() % 100);	
-    	uint32 total = 100 - rate_distribution[firstPos];
+    	uint32 rd = uint32(random() % 10000);
+    	uint32 total = 10000 - rate_distribution[firstPos];
     	uint32 begin = 0;
     	uint32 variation_attr = firstPos;
 		for(i=0; i<pos.length; i++) {
